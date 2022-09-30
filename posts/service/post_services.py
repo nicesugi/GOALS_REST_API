50ede94c64b5bf34dfc00f109445d49c269945e5
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-10-01 03:07:39 +0900

    feat/ 게시글 작성 기능 구현

---
+++ posts/service/post_services.py
@@ -0,0 +1,19 @@
+from posts.serializers import PostSerializer
+from posts.models import Post
+
+def create_post(create_data):
+    """
+    Args:
+        "writer" : post의 writer,
+        "title" : post의 title,
+        "content" : post의 content,
+        "tags" : post의 hashtags, 예시)"#제목,#내용,#태그"
+    """
+    post_data_serializer = PostSerializer(data=create_data)
+    post_data_serializer.is_valid(raise_exception=True)
+    post_data_serializer.save()
+
+    tags_data_list = create_data['tags'].replace(',' , '').split('#')
+    del tags_data_list[0]
+    for tag in tags_data_list:
+        Post.objects.last().tags.add(tag)
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from posts.service.post_services import (
+    create_post,
+)
+
+class PostView(APIView):
+    def post(self, request):
+        create_post(request.data)
+        return Response({'detail': '게시글이 작성되었습니다'}, status=status.HTTP_201_CREATED)
