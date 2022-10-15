193d83e4c364bef13757193c1952ce5face9c647
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-10-15 21:32:28 +0900

    test/ 게시글 수정하는 edit_post service 검증

--- posts/tests/test_post_services.py
+++ posts/tests/test_post_services.py
@@ -6,6 +6,7 @@ from users.models import User
 from posts.models import Post
 from posts.services.post_services import (
     create_post,
+    edit_post,
 )
 
 class TestService(TestCase):
@@ -15,8 +16,14 @@ class TestService(TestCase):
             username = 'test_user',
             email = 'test_email@naver.com',
             password = 'test_pw'
-        )
+            )
         
+        created_data = Post.objects.create(
+            writer = user,
+            title = 'test_title',
+            content = 'test_content',
+            tags = '#sns, #like, #post'
+            )
     def test_create_post(self):
         """
         게시물을 작성하는 create_post service 검증
@@ -27,7 +34,8 @@ class TestService(TestCase):
         create_data = {
             'title' : 'test_title',
             'content' : 'test_content',
-            'tags' : '#sns, #like, #post'}
+            'tags' : '#sns, #like, #post'
+            }
         # with CaptureQueriesContext(connection) as ctx:
         #     create_post(create_data, user)
         # ctx.captured_queries
@@ -43,7 +51,8 @@ class TestService(TestCase):
         create_data = {
             'title' : 'test_title',
             'content' : 'test_content',
-            'tags' : '#sns, #like, #post'}
+            'tags' : '#sns, #like, #post'
+            }
         with self.assertRaises(TypeError):
             create_post(create_data)
             
@@ -67,7 +76,8 @@ class TestService(TestCase):
         create_data = {
             'title' : '',
             'content' : 'test_content',
-            'tags' : '#sns, #like, #post'}
+            'tags' : '#sns, #like, #post'
+            }
         with self.assertRaises(exceptions.ValidationError):
             create_post(create_data, user)
             
@@ -81,7 +91,8 @@ class TestService(TestCase):
         create_data = {
             'title' : 'test_title',
             'content' : '',
-            'tags' : '#sns, #like, #post'}
+            'tags' : '#sns, #like, #post'
+            }
         with self.assertRaises(exceptions.ValidationError):
             create_post(create_data, user)
 
@@ -95,8 +106,30 @@ class TestService(TestCase):
         create_data = {
             'title' : 'test_title',
             'content' : 'test_content',
-            'tags' : ''}
+            'tags' : ''
+            }
         all_post_count = Post.objects.all().count()
         create_post(create_data, user)
         after_all_post_count = Post.objects.all().count()
         self.assertEqual(all_post_count, after_all_post_count-1)
+
+    def test_edit_post(self):
+        """
+        게시물을 수정하는 edit_post service 검증
+        case : 정상적으로 작동 했을 경우
+        result : 정상/바꾸고자하는 데이터와 바뀐 결과값이 같은지 확인
+        """
+        user = User.objects.get(username = 'test_user')
+        post = Post.objects.get(writer = user, title = 'test_title')
+
+        edit_data = {
+            'writer' : user.id,
+            'title' : 'test_edit_title',
+            'content' : 'test_edit_content',
+            'tags' : '#edit_sns, #edit_like, #edit_post'
+            }
+                
+        with self.assertNumQueries(39):
+            edit_post(edit_data, user, post.id)
+        edited_post = Post.objects.get(id = post.id)
+        self.assertEqual(edit_data['title'], edited_post.title)
