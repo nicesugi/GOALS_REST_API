ab1e8da03ea5f76deff066fbd94e3c8a8de0e08e
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-10-15 22:30:07 +0900

    test/ 게시글 수정하는 edit_post service 검증

--- posts/tests/test_post_services.py
+++ posts/tests/test_post_services.py
@@ -24,6 +24,7 @@ class TestService(TestCase):
             content = 'test_content',
             tags = '#sns, #like, #post'
             )
+        
     def test_create_post(self):
         """
         게시물을 작성하는 create_post service 검증
@@ -121,15 +122,101 @@ class TestService(TestCase):
         """
         user = User.objects.get(username = 'test_user')
         post = Post.objects.get(writer = user, title = 'test_title')
-
         edit_data = {
-            'writer' : user.id,
             'title' : 'test_edit_title',
             'content' : 'test_edit_content',
             'tags' : '#edit_sns, #edit_like, #edit_post'
             }
-                
-        with self.assertNumQueries(39):
+        edit_post(edit_data, user, post.id)
+        edited_post = Post.objects.get(id = post.id)
+        self.assertEqual(edit_data['title'], edited_post.title)
+
+    def test_fail_edit_post_without_arg_user(self):
+        """
+        게시물을 수정하는 edit_post service 검증
+        case : 인자 값 중 user가 들어오지 않을 경우 
+        result : 실패/TypeError 발생
+        """
+        post = Post.objects.get(title = 'test_title', content = 'test_content')
+        edit_data = {
+            'title' : 'test_edit_title',
+            'content' : 'test_edit_content',
+            'tags' : '#edit_sns, #edit_like, #edit_post'
+            }
+        with self.assertRaises(TypeError):
+            edit_post(edit_data, post.id)
+            
+    def test_fail_edit_post_without_arg_edit_data(self):
+        """
+            게시물을 수정하는 edit_post service 검증
+            case : 인자 값 중 edit_data가 들어오지 않을 경우 
+            result : 실패/TypeError 발생
+        """
+        user = User.objects.get(username = 'test_user')
+        post = Post.objects.get(writer = user, title = 'test_title')
+        with self.assertRaises(TypeError):
+            edit_post(user, post.id)
+            
+    def test_fail_edit_post_without_arg_post_id(self):
+        """
+        게시물을 수정하는 edit_post service 검증
+        case : 인자 값 중 post_id가 들어오지 않을 경우 
+        result : 실패/TypeError 발생
+        """
+        user = User.objects.get(username = 'test_user')
+        edit_data = {
+            'title' : 'test_edit_title',
+            'content' : 'test_edit_content',
+            'tags' : '#edit_sns, #edit_like, #edit_post'
+            }
+        with self.assertRaises(TypeError):
+            edit_post(edit_data, user)
+            
+    def test_fail_edit_post_without_title(self):
+        """
+        게시물을 수정하는 edit_post service 검증
+        case : title이 빈 값일 경우
+        result : 실패/ValidationError 발생
+        """
+        user = User.objects.get(username = 'test_user')
+        post = Post.objects.get(writer = user, title = 'test_title')
+        edit_data = {
+            'title' : '',
+            'content' : 'test_edit_content',
+            'tags' : '#edit_sns, #edit_like, #edit_post'
+            }
+        with self.assertRaises(exceptions.ValidationError):
             edit_post(edit_data, user, post.id)
+            
+    def test_fail_edit_post_without_content(self):
+        """
+        게시물을 수정하는 edit_post service 검증
+        case : content이 빈 값일 경우
+        result : 실패/ValidationError 발생
+        """
+        user = User.objects.get(username = 'test_user')
+        post = Post.objects.get(writer = user, title = 'test_title')
+        edit_data = {
+            'title' : 'test_edit_title',
+            'content' : '',
+            'tags' : '#edit_sns, #edit_like, #edit_post'
+            }
+        with self.assertRaises(exceptions.ValidationError):
+            edit_post(edit_data, user, post.id)
+
+    def test_edit_post_without_tags(self):
+        """
+        게시물을 수정하는 edit_post service 검증
+        case : tags가 빈 값일 경우
+        result : 정상/바꾸고자하는 데이터와 바뀐 결과값이 같은지 확인
+        """
+        user = User.objects.get(username = 'test_user')
+        post = Post.objects.get(writer = user, title = 'test_title')
+        edit_data = {
+            'title' : 'test_edit_title',
+            'content' : 'test_edit_content',
+            'tags' : ''
+            }
+        edit_post(edit_data, user, post.id)
         edited_post = Post.objects.get(id = post.id)
         self.assertEqual(edit_data['title'], edited_post.title)
