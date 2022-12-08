63cc75a76989d81572639e5f9b814eb261efcb4b
Superb777Sugi <sugi.28.jan@gmail.com>
2022-12-08 16:35:33 +0900

    enhan/ 회원가입 예외처리

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import exceptions, status
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -12,5 +12,15 @@ class UserView(APIView):
     """
 
     def post(self, request):
-        sign_up(request.data)
-        return Response({"detail": "회원가입을 성공하였습니다"}, status=status.HTTP_201_CREATED)
+        try:
+            sign_up(request.data)
+            return Response({"detail": "회원가입을 성공하였습니다"}, status=status.HTTP_201_CREATED)
+        except exceptions.ValidationError as e:
+            error = "\n".join(
+                [str(value) for values in e.detail.values() for value in values]
+            )
+            return Response({"detail": error}, status=status.HTTP_400_BAD_REQUEST)
+        except TypeError:
+            return Response(
+                {"detail": "작성내용을 확인해주세요"}, status=status.HTTP_404_NOT_FOUND
+            )
