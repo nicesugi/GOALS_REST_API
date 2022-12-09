bf7310891334c1bf038e4f4ca995fb8772f9071d
Superb777Sugi <sugi.28.jan@gmail.com>
2022-12-10 00:24:31 +0900

    refac/ body에 create_data 값이 None일 경우 조건 if문 추가

--- users/views.py
+++ users/views.py
@@ -1,5 +1,4 @@
 from rest_framework import exceptions, status
-from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -12,6 +11,10 @@ class UserView(APIView):
     """
 
     def post(self, request):
+        if request.data == {}:
+            return Response(
+                {"detail": "입력받은 값이 없습니다. 다시 확인해주세요"}, status=status.HTTP_404_NOT_FOUND
+            )
         try:
             sign_up(request.data)
             return Response({"detail": "회원가입을 성공하였습니다"}, status=status.HTTP_201_CREATED)
@@ -19,8 +22,12 @@ class UserView(APIView):
             error = "\n".join(
                 [str(value) for values in e.detail.values() for value in values]
             )
+            if "blank일 수 없습니다" in error:
+                error = "\n".join(
+                    [
+                        (keys + value.split("필드")[1])
+                        for keys, values in e.detail.items()
+                        for value in values
+                    ]
+                )
             return Response({"detail": error}, status=status.HTTP_400_BAD_REQUEST)
-        except TypeError:
-            return Response(
-                {"detail": "작성내용을 확인해주세요"}, status=status.HTTP_404_NOT_FOUND
-            )
